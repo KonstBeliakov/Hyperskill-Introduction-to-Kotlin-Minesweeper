b260773401c6f59a9685305225fa1de11dfd6e74
KonstBeliakov <konstbeliakov@gmail.com>
2025-01-26 16:28:50 +0300

    added ability to free cells

--- Minesweeper (Kotlin)/task/src/minesweeper/Main.kt
+++ Minesweeper (Kotlin)/task/src/minesweeper/Main.kt
@@ -13,6 +13,18 @@ fun displayBoard(board: MutableList<MutableList<Char>>) {
     println("—│—————————│")
 }
 
+fun freeCell(board: MutableList<MutableList<Char>>, x: Int, y: Int, shownBoard: MutableList<MutableList<Char>>) {
+    shownBoard[x][y] = if (board[x][y] == '.') '/' else board[x][y]
+    if(board[x][y] == '.' && x > 0 && x < 10 && y > 0 && y < 10){
+        for(i in -1..1){
+            for(j in -1..1){
+                if(shownBoard[x + i][y + j] == '.')
+                    freeCell(board, x + i, y + j, shownBoard)
+            }
+        }
+    }
+}
+
 fun main() {
     println("How many mines do you want on the field?")
     val mines = readln().toInt()
@@ -44,9 +56,7 @@ fun main() {
     }
 
     val shownBoard = MutableList(9 + 2) { i ->
-        MutableList(9 + 2) { j ->
-            if (board[i][j] == 'X') '.' else board[i][j]
-        }
+        MutableList(9 + 2) { j -> '.' }
     }
 
     var flags = 0
@@ -54,18 +64,35 @@ fun main() {
 
     while(!gameOver) {
         displayBoard(shownBoard)
-        println("Set/delete mines marks (x and y coordinates):")
+        println("Set/unset mine marks or claim a cell as free:")
         val line = readln().split(" ")
         val x = line[0].toInt()
         val y = line[1].toInt()
-        if (board[x][y].isDigit()) {
-            println("There is a number here!")
-        } else if (shownBoard[x][y] == '*') {
-            shownBoard[x][y] = '.'
-            flags--
-        } else {
-            shownBoard[x][y] = '*'
-            flags++
+        val command = line[2]
+
+        when (command) {
+            "free" -> {
+                if (board[x][y] == 'X') {
+                    println("You stepped on a mine and failed!")
+                    gameOver = true
+                } else {
+                    freeCell(board, x, y, shownBoard)
+                }
+            }
+
+            "mine" -> {
+                if (board[x][y].isDigit()) {
+                    println("There is a number here!")
+                } else if (shownBoard[x][y] == '*') {
+                    shownBoard[x][y] = '.'
+                    flags--
+                } else {
+                    shownBoard[x][y] = '*'
+                    flags++
+                }
+            }
+
+            else -> println("Wrong command")
         }
 
         if (flags == mines) {
